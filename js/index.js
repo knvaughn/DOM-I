b56b2f198681402cec7fe473e1363c41d914db1a
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-01-13 21:37:32 -0500

    updated nav, h1, and cta

--- js/index.js
+++ js/index.js
@@ -40,3 +40,11 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navLinks = document.querySelectorAll('nav a');
+for(let i = 0; i < navLinks.length; i++) {
+  navLinks[i].textContent = siteContent.nav[`nav-item-${i}`];
+}
+
+document.querySelector('h1').innerHTML = "DOM<br> Is<br> Awesome";
+document.querySelector('.cta button').textContent = "Get Started";
